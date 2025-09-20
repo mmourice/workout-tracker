aaa999ffa1c9c939aec5a1f64d727fcf4f16f4b7
mmourice <minamouricee@gmail.com>
2025-09-20 14:11:09 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.open("offline-cache").then(async (cache) => {
+      try {
+        const response = await fetch(event.request);
+        cache.put(event.request, response.clone());
+        return response;
+      } catch {
+        return cache.match(event.request);
+      }
+    })
+  );
+});
