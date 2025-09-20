9426708f2308aa58992b9c3f930bec2ab6feb96b
mmourice <minamouricee@gmail.com>
2025-09-20 23:19:54 +0300

    Update Plan.jsx

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStore } from "../store.jsx";
-import { TrashIcon } from "../components/Icons.jsx";
+import { TrashIcon, PlusIcon } from "../Icons.jsx";
 import { useNavigate } from "react-router-dom";
 
 export default function Plan() {
