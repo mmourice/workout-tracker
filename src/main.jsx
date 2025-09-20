61348785ec9a31d9d6468e1b5b9731b1cf715965
mmourice <minamouricee@gmail.com>
2025-09-20 23:12:11 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-- import { BrowserRouter } from "react-router-dom";
-+ import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles.css";
 
-- <BrowserRouter>
-+ <HashRouter>
-    <App />
-- </BrowserRouter>
-+ </HashRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
