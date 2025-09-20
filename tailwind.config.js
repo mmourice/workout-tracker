fd418f1be8a1099d80410d213674acb1da02fd4e
mmourice <minamouricee@gmail.com>
2025-09-20 15:06:23 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
@@ -33,4 +33,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
