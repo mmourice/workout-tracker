c678708d5cc5723f6a9e5d18d67ea6a21a516917
mmourice <minamouricee@gmail.com>
2025-09-20 22:00:02 +0300

    Create Icons.jsx

---
+++ src/Icons.jsx
@@ -0,0 +1,20 @@
+// src/components/Icons.jsx
+import React from "react";
+
+export const TrashIcon = ({ className = "w-5 h-5" }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="white"
+    strokeWidth="1.6"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    aria-hidden="true"
+  >
+    <path d="M3 6h18" />
+    <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+    <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
+    <path d="M10 11v6M14 11v6" />
+  </svg>
+);
