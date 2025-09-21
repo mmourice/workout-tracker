532c69f7d6a6c6e03412ec519cd330ca0992443e
mmourice <minamouricee@gmail.com>
2025-09-21 17:54:13 +0300

    Update Icons.jsx

--- src/Icons.jsx
+++ src/Icons.jsx
@@ -4,14 +4,16 @@ export function TrashIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
       className="h-5 w-5"
-      viewBox="0 0 20 20"
-      fill="currentColor"
     >
       <path
-        fillRule="evenodd"
-        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2h12a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zm-3 6a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zm5 0a1 1 0 011 1v7a1 1 0 11-2 0V9a1 1 0 011-1zm5-3a1 1 0 011 1v10a2 2 0 01-2 2H5a2 2 0 01-2-2V6a1 1 0 112 0v10h10V6a1 1 0 011-1z"
-        clipRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 7.5h12m-9 3v6m6-6v6M9 4.5h6a.75.75 0 01.75.75V6H8.25v-.75c0-.414.336-.75.75-.75zM4.5 6h15a.75.75 0 01.75.75v.75a.75.75 0 01-.75.75H4.5a.75.75 0 01-.75-.75v-.75a.75.75 0 01.75-.75zM6.75 9h10.5v10.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V9z"
       />
     </svg>
   );
@@ -21,14 +23,35 @@ export function PlusIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
       className="h-5 w-5"
-      viewBox="0 0 20 20"
-      fill="currentColor"
     >
       <path
-        fillRule="evenodd"
-        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-        clipRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+}
+
+export function ExternalIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-5 w-5"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M18 13.5V18a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 18V8.25A2.25 2.25 0 016.75 6H11M15.75 3h4.5m0 0v4.5m0-4.5L10.5 12"
       />
     </svg>
   );
