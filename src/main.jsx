b35c49286c6b7bfbbd6e618d9141301f388a7ca5
mmourice <minamouricee@gmail.com>
2025-09-22 12:38:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
+import ErrorBoundary from "./ErrorBoundary.jsx"; // ⬅️ NEW
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ErrorBoundary>      {/* ⬅️ NEW */}
+        <App />
+      </ErrorBoundary>
     </HashRouter>
   </React.StrictMode>
 );
